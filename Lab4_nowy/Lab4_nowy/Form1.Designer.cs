e10081f13f8c0c5f6616359cc7fa5f0739b08580
Jurand <handschuh.jurand@gmail.com>
2025-03-30 14:09:55 +0200

    gotowy_czerwony_niebieski

--- Lab4_nowy/Lab4_nowy/Form1.Designer.cs
+++ Lab4_nowy/Lab4_nowy/Form1.Designer.cs
@@ -30,6 +30,12 @@
         {
             pictureBox1 = new PictureBox();
             button_load = new Button();
+            stopnie1 = new RadioButton();
+            stopnie2 = new RadioButton();
+            stopnie3 = new RadioButton();
+            button_rotate = new Button();
+            button_invert_colors = new Button();
+            button_up_side_down = new Button();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
@@ -52,22 +58,105 @@
             button_load.UseVisualStyleBackColor = true;
             button_load.Click += button_load_Click;
             // 
+            // stopnie1
+            // 
+            stopnie1.AutoSize = true;
+            stopnie1.Location = new Point(12, 28);
+            stopnie1.Name = "stopnie1";
+            stopnie1.Size = new Size(64, 29);
+            stopnie1.TabIndex = 2;
+            stopnie1.TabStop = true;
+            stopnie1.Text = "90°";
+            stopnie1.UseVisualStyleBackColor = true;
+            stopnie1.CheckedChanged += stopnie1_CheckedChanged;
+            // 
+            // stopnie2
+            // 
+            stopnie2.AutoSize = true;
+            stopnie2.Location = new Point(12, 63);
+            stopnie2.Name = "stopnie2";
+            stopnie2.Size = new Size(74, 29);
+            stopnie2.TabIndex = 3;
+            stopnie2.TabStop = true;
+            stopnie2.Text = "180°";
+            stopnie2.UseVisualStyleBackColor = true;
+            stopnie2.CheckedChanged += stopnie2_CheckedChanged;
+            // 
+            // stopnie3
+            // 
+            stopnie3.AutoSize = true;
+            stopnie3.Location = new Point(12, 98);
+            stopnie3.Name = "stopnie3";
+            stopnie3.Size = new Size(74, 29);
+            stopnie3.TabIndex = 4;
+            stopnie3.TabStop = true;
+            stopnie3.Text = "270°";
+            stopnie3.UseVisualStyleBackColor = true;
+            stopnie3.CheckedChanged += stopnie3_CheckedChanged;
+            // 
+            // button_rotate
+            // 
+            button_rotate.BackColor = Color.FromArgb(255, 128, 128);
+            button_rotate.Location = new Point(12, 148);
+            button_rotate.Name = "button_rotate";
+            button_rotate.Size = new Size(184, 61);
+            button_rotate.TabIndex = 5;
+            button_rotate.Text = "Rotate";
+            button_rotate.UseVisualStyleBackColor = false;
+            button_rotate.Click += button_rotate_Click;
+            // 
+            // button_invert_colors
+            // 
+            button_invert_colors.BackColor = SystemColors.ActiveCaption;
+            button_invert_colors.Location = new Point(12, 215);
+            button_invert_colors.Name = "button_invert_colors";
+            button_invert_colors.Size = new Size(184, 61);
+            button_invert_colors.TabIndex = 6;
+            button_invert_colors.Text = "Invert Colors";
+            button_invert_colors.UseVisualStyleBackColor = false;
+            button_invert_colors.Click += button_invert_colors_Click;
+            // 
+            // button_up_side_down
+            // 
+            button_up_side_down.BackColor = SystemColors.ActiveCaption;
+            button_up_side_down.Location = new Point(12, 282);
+            button_up_side_down.Name = "button_up_side_down";
+            button_up_side_down.Size = new Size(184, 61);
+            button_up_side_down.TabIndex = 7;
+            button_up_side_down.Text = "Up Side Down";
+            button_up_side_down.UseVisualStyleBackColor = false;
+            button_up_side_down.Click += button_up_side_down_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1108, 661);
+            Controls.Add(button_up_side_down);
+            Controls.Add(button_invert_colors);
+            Controls.Add(button_rotate);
+            Controls.Add(stopnie3);
+            Controls.Add(stopnie2);
+            Controls.Add(stopnie1);
             Controls.Add(button_load);
             Controls.Add(pictureBox1);
             Name = "Form1";
             Text = "Edytor zdjec";
+            Load += Form1_Load;
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private PictureBox pictureBox1;
         private Button button_load;
+        private RadioButton stopnie1;
+        private RadioButton stopnie2;
+        private RadioButton stopnie3;
+        private Button button_rotate;
+        private Button button_invert_colors;
+        private Button button_up_side_down;
     }
 }
--- Lab4_nowy/Lab4_nowy/Form1.cs
+++ Lab4_nowy/Lab4_nowy/Form1.cs
@@ -2,6 +2,7 @@ namespace Lab4_nowy
 {
     public partial class Form1 : Form
     {
+        private int stopnie;
         public Form1()
         {
             InitializeComponent();
@@ -24,6 +25,86 @@ namespace Lab4_nowy
                 Bitmap bitmapa = new Bitmap(ofd.FileName);
                 pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
                 pictureBox1.Image = bitmapa;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void stopnie1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void stopnie2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void stopnie3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_rotate_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1 != null)
+            {
+                Bitmap orginalny = (Bitmap)pictureBox1.Image;
+                Bitmap zrotowany = new Bitmap(orginalny);
+
+                if (stopnie1.Checked)
+                {
+                    zrotowany.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                }
+                if (stopnie2.Checked)
+                {
+                    zrotowany.RotateFlip(RotateFlipType.Rotate180FlipNone);
+                }
+                if (stopnie3.Checked)
+                {
+                    zrotowany.RotateFlip(RotateFlipType.Rotate270FlipNone);
+                }
+
+                pictureBox1.Image = zrotowany;
+                orginalny.Dispose();
+            }
+        }
+
+        private void button_invert_colors_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                Bitmap orginalny = (Bitmap)pictureBox1.Image;
+                Bitmap negatyw = new Bitmap(orginalny.Width, orginalny.Height);
+
+                for (int x = 0; x < orginalny.Width; x++)
+                {
+                    for (int y = 0; y < orginalny.Height; y++)
+                    {
+                        Color pixel = orginalny.GetPixel(x, y);
+                        negatyw.SetPixel(x, y, Color.FromArgb(pixel.A, 255 - pixel.R, 255 - pixel.G, 255 - pixel.B));
+                    }
+                }
+
+                pictureBox1.Image = negatyw;
+                orginalny.Dispose();
+            }
+        }
+
+        private void button_up_side_down_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                Bitmap orginalny = (Bitmap)pictureBox1.Image;
+                Bitmap odwrocony = new Bitmap(orginalny);
+
+                odwrocony.RotateFlip(RotateFlipType.RotateNoneFlipY);
+
+                pictureBox1.Image = odwrocony;
+                orginalny.Dispose();
             }
         }
     }
