ed922c5044f8fe007a5300b9a788cd373a76019b
Jurand <handschuh.jurand@gmail.com>
2025-04-08 15:08:49 +0200

    gotowy_XML

--- Lab3/Lab3/obj/Debug/net9.0-windows/Lab3.AssemblyInfo.cs
+++ Lab3/Lab3/obj/Debug/net9.0-windows/Lab3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e87da36d3459b3cfff2cd3da0721d025f3131a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+160b5fb64af98e1fffa53086fdaced72963e1bb8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
