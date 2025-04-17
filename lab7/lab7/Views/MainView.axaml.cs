c10045a85950d6a8a47fba732902fc0d86b08158
Jurand <handschuh.jurand@gmail.com>
2025-04-17 13:02:37 +0200

    praca na zajeciach

---
+++ lab7/lab7/Views/MainView.axaml.cs
@@ -0,0 +1,34 @@
+﻿using Avalonia.Controls;
+using Avalonia.Interactivity;
+using System.Diagnostics;
+
+namespace lab7.Views;
+
+public partial class MainView : UserControl
+{
+    private string seq = "";
+    public MainView()
+    {
+        InitializeComponent();
+    }
+
+    private void Button_on_click(object? sender, RoutedEventArgs e)
+    {
+        Wynik.Text = seq;
+    }
+
+    private void Wczytana_sekwencja(object? sender, TextChangedEventArgs e)
+    {
+        if (sender is TextBox textBox)
+        {
+            seq = textBox.Text;
+        }
+    }
+
+    //DLA KAZDYCH 4 NUKLEOTYDOW, BIERZEMY PIERWSZE 4 I PATRZYMY CZY MAMY W LISCIE/SLOWNIKU ITD
+    private void Analiza(object? sender, RoutedEventArgs e)
+    {
+
+    }
+
+}
